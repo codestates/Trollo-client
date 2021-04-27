e42a108bb8aad134c7095b96f923c97e04546660
useonglee <useong0830@naver.com>
2021-04-27 23:49:43 +0900

    Add #2 add Router Login & Register

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,21 @@ import React from 'react';
 import { Global, ThemeProvider } from '@emotion/react';
 import GlobalStyle from './styles/GlobalStyel';
 import Theme from './styles/Theme';
+import Login from './Pages/LogIn/LogIn';
+import Register from './Pages/LogIn/Register';
+import Workspace from './Pages/Workspace/index';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 const App = (): JSX.Element => {
 	return (
 		<ThemeProvider theme={Theme}>
 			<Global styles={GlobalStyle} />
+			<Switch>
+				<Redirect exact path="/" to="/Login" />
+				<Route path="/Login" component={Login} />
+				<Route path="/Register" component={Register} />
+				<Route path="/Workspace/:Workspace" component={Workspace} />
+			</Switch>
 		</ThemeProvider>
 	);
 };
