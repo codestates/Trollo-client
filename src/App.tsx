45a0d1b83c70c2c2481fc7b6f7bc188d7de870e8
Woogie-94 <keu1106@naver.com>
2021-04-28 21:07:40 +0900

    Add Worksapce Route

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,16 @@ import React from 'react';
 import { Global, ThemeProvider } from '@emotion/react';
 import GlobalStyle from './styles/GlobalStyel';
 import Theme from './styles/Theme';
+import { Route, Switch } from 'react-router-dom';
+import Worksapce from './pages/Workspace';
 
 const App = (): JSX.Element => {
 	return (
 		<ThemeProvider theme={Theme}>
 			<Global styles={GlobalStyle} />
+			<Switch>
+				<Route path="/workspace" component={Worksapce} />
+			</Switch>
 		</ThemeProvider>
 	);
 };
