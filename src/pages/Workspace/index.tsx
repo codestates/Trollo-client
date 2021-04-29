41dadf85fe7c85e1cc037759421a26f818d0bb16
Woogie-94 <keu1106@naver.com>
2021-04-29 16:13:22 +0900

    Add Navbar component

--- src/pages/Workspace/index.tsx
+++ src/pages/Workspace/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import DragAndDropArea from '../../components/Workspace/DragAndDropArea';
+import Navbar from '../../components/Common/Navbar';
 
 const Worksapce = (): JSX.Element => {
 	return (
 		<>
+			<Navbar />
 			<DragAndDropArea />
 		</>
 	);
