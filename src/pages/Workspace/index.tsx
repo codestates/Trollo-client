a66375a01c38e0bb80838bf531e19483a6b38811
useonglee <useong0830@naver.com>
2021-05-02 23:42:06 +0900

    Set #24 Remarking Code

--- src/pages/Workspace/index.tsx
+++ src/pages/Workspace/index.tsx
@@ -2,14 +2,13 @@ import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import DragAndDropArea from '../../components/Workspace/DragAndDropArea';
 import Navbar from '../../components/Common/Navbar';
-import { getAccessTokenSelector } from '../../reducer/accessToken';
 
 const Worksapce = (): JSX.Element => {
-	const accessTokenState = useSelector(getAccessTokenSelector);
+	// const accessTokenState = useSelector(getAccessTokenSelector);
 
-	useEffect(() => {
-		console.log('1111111111111', accessTokenState);
-	}, [accessTokenState]);
+	// useEffect(() => {
+	// 	console.log('1111111111111', accessTokenState);
+	// }, [accessTokenState]);
 	return (
 		<>
 			<Navbar />
