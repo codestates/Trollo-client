5c4e7e5c3a0a63ec2b3ff35fad52a9632db07d01
Woogie-94 <keu1106@naver.com>
2021-05-02 23:35:42 +0900

    Remove useSelector test code delete

--- src/pages/Workspace/index.tsx
+++ src/pages/Workspace/index.tsx
@@ -1,15 +1,8 @@
-import React, { useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import React from 'react';
 import DragAndDropArea from '../../components/Workspace/DragAndDropArea';
 import Navbar from '../../components/Common/Navbar';
-import { getAccessTokenSelector } from '../../reducer/accessToken';
 
 const Worksapce = (): JSX.Element => {
-	const accessTokenState = useSelector(getAccessTokenSelector);
-
-	useEffect(() => {
-		console.log('1111111111111', accessTokenState);
-	}, [accessTokenState]);
 	return (
 		<>
 			<Navbar />
