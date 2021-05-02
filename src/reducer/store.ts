4881c5ce0b6d3b616c453e51c2db8b4a438cd8ed
Woogie-94 <keu1106@naver.com>
2021-05-02 23:33:28 +0900

    Add #36 workspace status management

--- src/reducer/store.ts
+++ src/reducer/store.ts
@@ -3,12 +3,14 @@ import { configureStore, getDefaultMiddleware, combineReducers } from '@reduxjs/
 import { contentSlice, selectedContentSlice } from './board';
 // import { mailLogin } from './authorization';
 import getAccessToken from './accessToken';
+import { taskSlice } from './workspace';
 
 export const reducer = combineReducers({
 	content: contentSlice.reducer,
 	selectedContent: selectedContentSlice.reducer,
 	// auth: mailLogin.reducer,
 	getAccessToken,
+	TaskData: taskSlice.reducer,
 });
 
 const middleware = [...getDefaultMiddleware(), logger];
