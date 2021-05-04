881d4c2e1a2e4f0f80e72e3a9af664af0e0464d8
Woogie-94 <keu1106@naver.com>
2021-05-04 23:47:11 +0900

    Set changed Route component name

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,24 @@ import React from 'react';
 import { Global, ThemeProvider } from '@emotion/react';
 import GlobalStyle from './styles/GlobalStyel';
 import Theme from './styles/Theme';
-import Login from './pages/LogIn/LogIn';
-import Register from './pages/LogIn/Register';
-import Board from './pages/Board/index';
-import Content from './pages/Content/index';
+import login from './pages/LogIn/LogIn';
+import register from './pages/LogIn/Register';
+import board from './pages/Board/index';
+import content from './pages/Content/index';
 import { Switch, Route, Redirect } from 'react-router-dom';
-import Worksapce from './pages/Workspace';
+import worksapce from './pages/Workspace';
 
 const App = (): JSX.Element => {
 	return (
 		<ThemeProvider theme={Theme}>
 			<Global styles={GlobalStyle} />
 			<Switch>
-				<Redirect exact path="/" to="/Login" />
-				<Route path="/Login" component={Login} />
-				<Route path="/Register" component={Register} />
-				<Route exact path="/Board" component={Board} />
-				<Route path="/Board/:id" component={Content} />
-				<Route path="/workspace" component={Worksapce} />
+				<Redirect exact path="/" to="/login" />
+				<Route path="/Login" component={login} />
+				<Route path="/Register" component={register} />
+				<Route exact path="/Board" component={board} />
+				<Route path="/Board/:id" component={content} />
+				<Route path="/workspace" component={worksapce} />
 			</Switch>
 		</ThemeProvider>
 	);
