66764f9f979d067906b8d85826439927ba2b9d72
useonglee <useong0830@naver.com>
2021-05-04 23:18:10 +0900

    Add #51 add Board detail page

---
+++ src/components/BoardDetail/index.tsx
@@ -0,0 +1,82 @@
+import React, { useEffect } from 'react';
+import { useHistory, useParams } from 'react-router';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+	axiosContentDetail,
+	axiosContentDetailDelete,
+	getContentDetailData,
+} from '../../reducer/board_detail';
+import gravatar from 'gravatar';
+import {
+	Container,
+	ContentCreatedAt,
+	ContentDelete,
+	ContentView,
+	ContentWrapper,
+	ContentWriter,
+	ContentTitle,
+	ContentMainBoard,
+	ContentWirterProfile,
+	ContetnUserInfo,
+	UpdataButton,
+	ContentEdit,
+} from './styles';
+import { getLoginInfoSelector } from '../../reducer/accessToken';
+import FakeBoard from '../../images/FakeBoard.png';
+// interface Props {
+//     match: number,
+// }
+
+const BoardDetail = (): JSX.Element => {
+	const history = useHistory();
+	const contentData = useSelector(getContentDetailData);
+	const userInfo = useSelector(getLoginInfoSelector);
+	const contentAccessToken = useSelector(getLoginInfoSelector);
+
+	const dispatch = useDispatch();
+
+	const { id } = useParams<{ id?: string }>();
+	const authorization = `Bearer ${contentAccessToken.accessToken}`;
+	const LoginType = contentAccessToken.LoginType;
+	const board_id = Number(id);
+	const writer = contentData.writer?.split('@')[0];
+	const date = contentData.createdAt?.split('T')[0];
+
+	useEffect(() => {
+		dispatch(axiosContentDetail(authorization, LoginType, board_id));
+	}, []);
+
+	const onDelete = (): void => {
+		dispatch(axiosContentDetailDelete(authorization, LoginType, board_id));
+		history.push('/board');
+		alert('해당 게시글이 삭제 되었습니다.');
+	};
+
+	return (
+		<Container>
+			<ContentWrapper>
+				<ContentTitle>{contentData.title}</ContentTitle>
+				<ContetnUserInfo>
+					<ContentWirterProfile
+						src={gravatar.url('trollo@gmail.com', { s: '32px', d: 'retro' })}
+						alt={'trollo'}
+					/>
+					<ContentWriter>{writer}</ContentWriter>
+					<ContentCreatedAt>{date}</ContentCreatedAt>
+				</ContetnUserInfo>
+			</ContentWrapper>
+
+			{contentData.writer === userInfo.email ? (
+				<UpdataButton>
+					<ContentEdit>수정</ContentEdit>
+					<ContentDelete onClick={onDelete}>삭제</ContentDelete>
+				</UpdataButton>
+			) : null}
+			<ContentMainBoard>
+				<img src={FakeBoard} />
+			</ContentMainBoard>
+		</Container>
+	);
+};
+
+export default BoardDetail;
---
+++ src/components/BoardDetail/styles.ts
@@ -0,0 +1,70 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	padding-top: 56px;
+	height: 1000px;
+	max-height: 1200px;
+`;
+
+export const ContentWrapper = styled.div`
+	width: 800px;
+	border-bottom: 0.5px solid #e9e9e9;
+`;
+
+export const ContetnUserInfo = styled.div`
+	display: flex;
+	align-items: center;
+	margin-bottom: 10px;
+`;
+
+export const ContentTitle = styled.div`
+	weight: 500px;
+	font-size: 40px;
+	margin-bottom: 10px;
+	padding: 80px 0px 40px 0px;
+
+	text-align: center;
+`;
+
+export const ContentWirterProfile = styled.img`
+	border-radius: 100%;
+`;
+
+export const ContentWriter = styled.span`
+	margin-left: 8px;
+	font-weight: 500;
+	color: #777777;
+`;
+
+export const ContentCreatedAt = styled.span`
+	margin-left: 12px;
+	color: #bebbd0;
+`;
+
+export const ContentView = styled.div``;
+
+export const UpdataButton = styled.div`
+	width: 800px;
+	margin-top: 10px;
+	text-align: right;
+`;
+
+export const ContentEdit = styled.span`
+	margin-right: 5px;
+	font-size: 14px;
+	cursor: pointer;
+`;
+
+export const ContentDelete = styled.span`
+	font-size: 14px;
+	color: #ea092e;
+	cursor: pointer;
+`;
+export const ContentMainBoard = styled.div`
+	margin-top: 70px;
+	width: 800px;
+	border-bottom: 0.5px solid #e9e9e9;
+`;
