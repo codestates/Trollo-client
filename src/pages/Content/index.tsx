54eaabcbb81a5a2eef0909b441fbb6d00b1490d8
useonglee <useong0830@naver.com>
2021-05-06 06:56:51 +0900

    Fix #59 remove Comment of Table

--- src/pages/Content/index.tsx
+++ src/pages/Content/index.tsx
@@ -1,32 +1,14 @@
 import React from 'react';
-import { useHistory, useParams } from 'react-router';
-// import { getContentById } from '../../components/Board/Table/FakeData';
-import { ContentWrapper, GobackButton, Header } from './styles';
 
-// interface Props {
-//     match: number,
-// }
+import BoardDetail from '../../components/BoardDetail';
+import Comment from '../../components/Comment';
+import Navbar from '../../components/Common/Navbar';
 
 const Content = (): JSX.Element => {
-	// const [data, setData] = useState({});
-	const history = useHistory();
-	const { id } = useParams<{ id?: string }>();
-
-	console.log(id);
-
-	// useEffect(() => {
-	//     setData(getContentById(id));
-	// }, [ ]);
-
-	const onGoback = () => {
-		history.push('/board');
-	};
-
 	return (
 		<>
-			<Header>게시글</Header>
-			<ContentWrapper></ContentWrapper>
-			<GobackButton onClick={onGoback}>목록으로 돌아가기</GobackButton>
+			<Navbar />
+			<BoardDetail />
 		</>
 	);
 };
--- src/pages/Content/styles.ts
+++ src/pages/Content/styles.ts
@@ -1,15 +1,3 @@
 import styled from '@emotion/styled';
 
 export const Header = styled.h2``;
-
-export const ContentWrapper = styled.div``;
-
-export const ContentNumber = styled.div``;
-
-export const ContentTitle = styled.div``;
-
-export const ContentView = styled.div``;
-
-export const ContentCreateAt = styled.div``;
-
-export const GobackButton = styled.button``;
