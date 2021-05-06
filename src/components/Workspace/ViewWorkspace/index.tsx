6a864f4ec94adf8135f7a390e3c2f17e3cb4c6c9
Woogie-94 <keu1106@naver.com>
2021-05-06 09:33:37 +0900

    Correct route path change

---
+++ src/components/Workspace/ViewWorkspace/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { WorksapceContainer } from '../DragAndDropArea/styles';
+
+const ViewWorkspace = (): JSX.Element => {
+	return (
+		<WorksapceContainer>
+			<div></div>
+		</WorksapceContainer>
+	);
+};
+export default ViewWorkspace;
